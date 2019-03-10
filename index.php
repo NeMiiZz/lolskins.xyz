aad411a2a2961b355dd7ee16ece0dbfb8bb74b0f
James Biltaji <jimilordofwar@hotmail.com>
2019-03-10 17:34:12 +0000

    removed the dragonhead dataset since it is quite out of date

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
-<!-- <! DOCTYPE html>
+<?php 
+	include("include/config.php");
+?>
 
-<?php $test = "this is a php string."; ?>
-
-<html>
-	<head>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<link rel="stylesheet" href="/public/bin/css/main.css">
-	</head>
-	<body>
-		<div class="jumbotron text-center">
-			<input type='text' id='lol-search-box'>
-		</div>
-		<div class="container text-center">
-			<div class="row">
-				<div class="col-lg-12">
-					<span id='lol-champion-icon'></span>
-					<h2 id='lol-champion-name'></h2>
-					<p id='lol-champion-desc'></p>
-				</div>
+<! DOCTYPE html>
+<?php if ($maintenance) { ?>
+	<html>
+		<body style='background-image:url(/public/images/maintenance.png);background-repeat: no-repeat;background-size: cover;' class="maintanence">
+		</body>
+	</html>
+<?php } else { ?>
+	<html>
+		<head>
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+			<link rel="stylesheet" href="/public/bin/css/main.css">
+		</head>
+		<body>
+			<div class="jumbotron text-center " id='slide-show'>
+				<h1 class="lol-home-title">LoL Skins</h1>
+				<input type='text' id='lol-search-box' placeholder="Search for a Champion or Skin name">
+			</div>
+			<div class="container text-center">
+				<span id='lol-champion-icon'></span>
+				<h2 id='lol-champion-name'></h2>
+				<p id='lol-champion-desc'></p>
+				<div class="row" id="lol-champion-list"></div>
 			</div>
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 			<script src="/public/bin/javascript/main.js"></script>
-		</div>
-	</body>
-</html>
--->
-
+		</body>
+	</html>
+<?php } ?>
--- public/bin/javascript/main.js
+++ public/bin/javascript/main.js
@@ -1,40 +1,83 @@
 $(document).ready( function() {
 
-  $('#lol-search-box').on('keyup', function() {
+	$('#lol-search-box').on('keyup', function() {
 
-    var searchVal = capitalizeFirstLetter($(this).val());
+		var searchVal = capitalizeFirstLetter($(this).val());
+		if (searchVal.length >= 3) {
 
-    $.post("/data/dragonhead/6.24.1/data/en_GB/champion/"+searchVal+".json").done(function(response) {
-      console.log(response);
-      if (response) {
-        // store champion data in variable
-        var champion = response;
-        var random = getRandomInt(0, champion.data[searchVal].enemytips.length);
-        console.log(random);
-        var championName = champion.data[searchVal].name;
-        var championDesc = champion.data[searchVal].enemytips[random];
-        // get the champion icon
-        $("#lol-champion-icon").html("<img src='/data/dragonhead/6.24.1/img/champion/"+championName+".png'></img>")
-        $("#lol-champion-name").html(championName);
-        $("#lol-champion-desc").html("<p>"+championDesc+"</p>");
-        $.post("/data/dragonhead/6.24.1/data/en_GB/profileicon.json")
-        .done(function(response) {
-        
-        });
-      }
-      
-    });
-  });
+			//$.post("http://ddragon.leagueoflegends.com/cdn/9.5.1/data/en_GB/champion/"+searchVal+".json").done(function(response) {
+				$.ajax({
+					type: 'GET',
+					crossDomain: true,
+					dataType: 'json',
+					url: 'http://ddragon.leagueoflegends.com/cdn/9.5.1/data/en_GB/champion/'+searchVal+'.json',
+					success: function(response) {
+					console.log(response);
+					// store champion data in variable
+					var champion = response;
+					var random = getRandomInt(0, champion.data[searchVal].enemytips.length);
+					console.log(random);
+					var championName = champion.data[searchVal].name;
+					var championDesc = champion.data[searchVal].enemytips[random];
+					// get the champion icon
+					$("#lol-champion-icon").html("<img src='http://ddragon.leagueoflegends.com/cdn/9.5.1/img/champion/"+championName+".png'></img>");
+					$("#lol-champion-name").html(championName);
+					$("#lol-champion-desc").html("<p>"+championDesc+"</p>");
+				}
+			});
+		}
+	});
+
+	$.ajax({
+		type: 'GET',
+		crossDomain: true,
+		dataType: 'json',
+		url: 'http://ddragon.leagueoflegends.com/cdn/9.5.1/data/en_GB/champion.json',
+		success: function(response) {
+			console.log(response);
+			$.each(response.data, function(index, element) {
+				console.log(index);
+				console.log(element);
+				$("#lol-champion-list").append(
+					"<div class='col-lg-1'>"+
+						"<a href='#' class='champ-pic-link'>"+
+							"<img src='http://ddragon.leagueoflegends.com/cdn/9.5.1/img/champion/"+index+".png' class='lol-champion-pic'>"+
+						"</a>"+
+					"</div>"
+				)
+			});
+		}
+	});
 });
 
+var slideIndex = 0;
+slideShow(slideIndex);
 
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function slideShow(slideIndex) {
+	var i;
+	var slides = $("#slide-show");
+	console.log(slideIndex);
+	
+
+	var images = ['Garen', 'Aatrox', 'Jax', 'Jinx', 'Teemo', 'Zed'];
+	
+
+	slides.css("background-image", "url(/data/dragonhead/img/champion/splash/"+images[slideIndex]+"_2.jpg)");
+	slides.addClass("lol-jumbo-background-image");
+
+	slideIndex++;
+
+	if (slideIndex > images.length) {slideIndex = 0} 
+//	setTimeout(slideShow(slideIndex), 5000); // Change image every 2 seconds
 }
